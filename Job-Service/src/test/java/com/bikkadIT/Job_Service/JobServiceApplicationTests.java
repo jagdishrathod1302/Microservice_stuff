b19260f8a2825be08e19ba17b9c269f7c7d8eec2
Jagdish <jagdishrj188653@gmail.com>
2025-06-24 19:03:28 +0530

    Info about Microservice

---
+++ Job-Service/src/test/java/com/bikkadIT/Job_Service/JobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikkadIT.Job_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
